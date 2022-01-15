03e27499550352cc55b70b2bd429bed955386727
Khoirunnisa' Rizky <khoirunnisa.rizky.n@mail.ugm.ac.id>
2022-01-15 07:05:11 +0700

    clean the code

--- hello.c
+++ hello.c
@@ -2,35 +2,6 @@
 
 int main(void)
 {
-    // string answer = get_string("what's your name?");
-    //printf("hello, %s\n", answer);
-
-    //int firstNumber = get_int("what's ur fisrt number?");
-    //int secondNumber = get_int("what's ur second number");
-    //printf("the result is %i\n", firstNumber + secondNumber);
-    //long firstNumber = get_long("what's ur first number?");
-    //long secondNumber = get_long("what's ur second number");
-    //addition
     printf("the result is %li\n", 5);
-
-    //comparing value
-   // if (firstNumber < secondNumber)
-    //{
-        printf("firstNumber is less than secondNumber\n");
-    //} else {
-    //    if (firstNumber > secondNumber)
-    //    {
-            printf("firstNumber is greater than secondNumber\n");
-   //     } else {
-    //        printf("firstNumber is equal with secondNumber\n");
-    //    }
-   // }
-
-    //comparing character answer
-    //char character = get_char("do you agree?");
-    //if (character == 'y') {
-        printf("agreed");
-    //} else {
-        printf("revoked");
-    //}
+    printf("firstNumber is less than secondNumber\n");
 }
